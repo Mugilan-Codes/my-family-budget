6768a2c7af68fca400d29e0f94be071dba32b110
Mugilan <mugilancodes@gmail.com>
2020-06-29 23:23:44 +0530

    Created env.js

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 
 // Dotenv files
-const { PORT } = require('./config');
+const { port } = require('./env');
 
 app.get('/', (req, res) => {
   res.send('API Running...');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
