46342a3a141a166ec1de8f6aba4d40e3092dd8b8
Mugilan <mugilancodes@gmail.com>
2020-07-10 12:48:39 +0530

    Initial Routing Setup

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ import cors from 'cors';
 const app = express();
 
 import { port } from './env';
+import mountRoutes from './routes';
 
 app.use(cors());
+mountRoutes(app);
 
 app.get('/', (req, res) => {
   res.send('API Running...');
