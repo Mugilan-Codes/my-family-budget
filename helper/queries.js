6b875b126f1ffb012ee6f50232b8244a38aa03c8
Mugilan <mugilancodes@gmail.com>
2020-07-11 20:40:40 +0530

    Login User

--- helper/queries.js
+++ helper/queries.js
@@ -2,32 +2,54 @@ import sqltag from 'sql-template-tag';
 
 import { pool } from '../config/pool';
 
-const findById = async (id) => {
-  const user = (
-    await pool.query(
-      sqltag`SELECT id, name, email, username, created_on, updated_on FROM users WHERE id = ${id}`
-    )
-  ).rows[0];
+const findById = async (id, passowrd = false) => {
+  let user;
+
+  if (passowrd) {
+    user = (await pool.query(sqltag`SELECT * FROM users WHERE id = ${id}`))
+      .rows[0];
+  } else {
+    user = (
+      await pool.query(
+        sqltag`SELECT id, name, email, username, created_on, updated_on FROM users WHERE id = ${id}`
+      )
+    ).rows[0];
+  }
 
   return user;
 };
 
-const findByEmail = async (email) => {
-  const user = (
-    await pool.query(
-      sqltag`SELECT id, name, email, username, created_on, updated_on FROM users WHERE email = ${email}`
-    )
-  ).rows[0];
+const findByEmail = async (email, password = false) => {
+  let user;
+
+  if (password) {
+    user = (
+      await pool.query(sqltag`SELECT * FROM users WHERE email = ${email}`)
+    ).rows[0];
+  } else {
+    user = (
+      await pool.query(
+        sqltag`SELECT id, name, email, username, created_on, updated_on FROM users WHERE email = ${email}`
+      )
+    ).rows[0];
+  }
 
   return user;
 };
 
-const findByUsername = async (username) => {
-  const user = (
-    await pool.query(
-      sqltag`SELECT id, name, email, username, created_on, updated_on FROM users WHERE username = ${username}`
-    )
-  ).rows[0];
+const findByUsername = async (username, password = false) => {
+  let user;
+  if (password) {
+    user = (
+      await pool.query(sqltag`SELECT * FROM users WHERE username = ${username}`)
+    ).rows[0];
+  } else {
+    user = (
+      await pool.query(
+        sqltag`SELECT id, name, email, username, created_on, updated_on FROM users WHERE username = ${username}`
+      )
+    ).rows[0];
+  }
 
   return user;
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import { check } from 'express-validator';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 import { auth } from '../../middleware/auth';
-import { findById } from '../../helper/queries';
+import { findById, findByEmail } from '../../helper/queries';
+import { validate } from '../../middleware/validator';
+import { jwt_secret } from '../../env';
 
 const router = express.Router();
 
@@ -15,5 +20,58 @@ router.get('/', auth, async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+/**
+ * @route          POST api/auth
+ * @desc           Authenticate user & Get token
+ * @access         Public
+ ** Request Body
+ * @param {string} email     Email of the user.
+ * @param {string} password  Password for the user.
+ */
+router.post(
+  '/',
+  validate([
+    check('email', 'Please include email').isEmail(),
+    check('password', 'Password required').exists(),
+  ]),
+  async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+      // todo - Get username as alternative for email
+      let user = await findByEmail(email, true);
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      console.log(password, user.password);
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(payload, jwt_secret, { expiresIn: '3d' }, (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 export default router;
