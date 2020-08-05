fbcc75a1d3bc306c36588949f29e17df0d92a1af
Mugilan <mugilancodes@gmail.com>
2020-08-05 19:38:44 +0530

    Changed token time to 3d instead of 1h

--- server/src/utils/token.js
+++ server/src/utils/token.js
@@ -2,8 +2,9 @@ import jwt from 'jsonwebtoken';
 
 import { jwt_secret } from '../config';
 
+//! Change back to '1h' during production
 const generateAccessToken = (payload) => {
-  return jwt.sign(payload, jwt_secret, { expiresIn: '1h' });
+  return jwt.sign(payload, jwt_secret, { expiresIn: '3d' });
 };
 
 const verifyToken = (token) => {
